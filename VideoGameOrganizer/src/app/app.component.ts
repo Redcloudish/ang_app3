baa18a12ce51ac3682c5e91d3a87bb981a13e41e
Michael Brown <160866598+Redcloudish@users.noreply.github.com>
2024-11-13 18:51:38 -0500

    new code

--- VideoGameOrganizer/src/app/app.component.ts
+++ VideoGameOrganizer/src/app/app.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-game-list></app-game-list>',
+  template: `
+    <div class="game-container">
+      <h1>Welcome to Video Game Organizer</h1>
+      <div *ngFor="let genre of genres">
+        <h2>{{ genre }}</h2>
+        <ul>
+          <li *ngFor="let game of getGamesByGenre(genre)">
+            {{ game.title }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./app.component.css'],  // This line links the CSS file
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  genres: string[] = ['Action', 'RPG', 'Strategy'];
+  games = [
+    { title: 'The Legend of Zelda', genre: 'Adventure' },
+    { title: 'Final Fantasy VII', genre: 'RPG' },
+    { title: 'Street Fighter V', genre: 'Action' },
+    { title: 'Civilization VI', genre: 'Strategy' },
+  ];
+
+  getGamesByGenre(genre: string) {
+    return this.games.filter(game => game.genre === genre);
+  }
+
+  ngOnInit(): void {}
+}
